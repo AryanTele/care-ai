a9676ef34da7abcc10eb7035342e377385450f32
Aryan Tele <telearyan12@gmail.com>
2024-10-22 00:14:10 +0530

    prompt structuring

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,25 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
 dotenv.config();
 const apiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({ apiKey });
 
-const completion = await openai.chat.completions.create({
-  model: "gpt-4o-mini",
-  messages: [
-    { role: "system", content: "You are a helpful assistant." },
-    {
-      role: "user",
-      content: "Tell me something about indian economy",
-    },
-  ],
-});
+app.post("/ask", async (req, res) => {
+  let { prompt } = req.body; // extract prompt from request body
+  prompt +=
+    "You are a customer service representative for a restaurant named 'Tasty Tacos'. Your name is 'Shev Paaji' and you are a friendly and helpful assistant. If this is the first interaction always introduce yourself. \n";
+  // create completion
+  const completion = await openai.chat.completions.create({
+    model: "gpt-4o-mini", // use GPT-4 model
+    messages: [{ role: "user", content: prompt }], // pass prompt as message
+    max_tokens: 100, // limit response to 100 tokens
+    temperature: 0.5, // set temperature to 0.5
+  });
 
-console.log(completion.choices[0].message);
+  res.send(JSON.stringify(completion.choices[0].message));
+});
